a23e8f4cfc41d84d2c2468fcf39b88a77d440329
cwret <cwret14@imperial.ac.uk>
2017-11-15 13:51:27 +0000

    Remove scripts dir

--- scripts/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(find ../NEUT_MIN -maxdepth 1 -name "*MINOO_SEP2017*outgoing*" -type f); do 
-  j=${i/MINOO/NEUT533}
-  j=${j/NEUT_MIN/NEUT533}
-  root -b -q -l 'makeratio.C("'${i}'", "'${j}'")';
-done
